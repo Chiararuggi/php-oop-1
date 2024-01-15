4aab1aa01d8778dd8c0e03e29dbce140c121c373
chiara.ruggi <chiara.ruggi1998@gmail.com>
2024-01-15 09:56:06 +0100

    var dump

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
-?>
+ $firstMovie = new Movie("The Departed", "Il dipartimento di polizia di Boston ha deciso di sgominare il boss mafioso Frank Costello e la sua gang.", 2006);
+ $secondMovie = new Movie("Il Grande Gatsby", "Un aspirante scrittore arriva a New York nel 1922 in cerca del sogno americano.", 2013);
+
+ var_dump($firstMovie);
+ var_dump($secondMovie);
+ ?>
